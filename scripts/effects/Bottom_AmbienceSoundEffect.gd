96107d01c881b1d2a1c8c81dc81868b3564aadbe
aryrk <amanwhohave@gmail.com>
2024-05-19 10:57:05 +0700

    add owl and rusting

---
+++ scripts/effects/Bottom_AmbienceSoundEffect.gd
@@ -0,0 +1,19 @@
+extends AudioStreamPlayer3D
+var timer
+
+func _ready():
+	timer = $SoundTimer
+	randomize_player_timeout()
+func randomize_player_timeout():
+	timer.wait_time = randf_range(30, 100)
+
+func _process(delta):
+	pass
+
+func _on_sound_timer_timeout():
+	if is_playing():
+		return
+	var audio_stream: AudioStream = load("assets/sounds/effect/Brush Rustling/Rust" + str(randi_range(1, 3)) + ".mp3")
+	set_stream(audio_stream)
+	play()
+	randomize_player_timeout()
---
+++ scripts/effects/Top_AmbienceSoundEffect.gd
@@ -0,0 +1,20 @@
+extends AudioStreamPlayer3D
+
+var timer
+
+func _ready():
+	timer = $SoundTimer
+	randomize_player_timeout()
+func randomize_player_timeout():
+	timer.wait_time = randf_range(45, 300)
+
+func _process(delta):
+	pass
+
+func _on_sound_timer_timeout():
+	if is_playing():
+		return
+	var audio_stream: AudioStream = load("assets/sounds/effect/Owl/owl" + str(randi_range(1, 3)) + ".mp3")
+	set_stream(audio_stream)
+	play()
+	randomize_player_timeout()
