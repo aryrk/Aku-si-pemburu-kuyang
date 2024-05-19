e9b1dba59c617f552e79005bcd0844c71899a259
Matharrr <117796799+Matharrr@users.noreply.github.com>
2024-05-19 12:27:55 +0700

    main

--- scripts/scene/mainMenu.gd
+++ scripts/scene/mainMenu.gd
@@ -1,17 +1,11 @@
 extends Node3D
-var main_scene = preload("res://scenes/main.tscn").instantiate()
 
+const MAIN = preload("res://scenes/main.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-func _input(event):
-	if event is InputEventKey:
-		if event.pressed:
-			
-			get_tree().change_scene_to_packed(main_scene)
+	if (Input.is_anything_pressed()==true):
+		get_tree().change_scene_to_packed(MAIN)
