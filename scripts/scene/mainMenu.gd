5f092fef2f2abf32a97701337714ddc10f1820c9
aryrk <amanwhohave@gmail.com>
2024-05-19 14:41:20 +0700

    implement main menu

--- scripts/scene/mainMenu.gd
+++ scripts/scene/mainMenu.gd
@@ -8,4 +8,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (Input.is_anything_pressed()==true):
-		get_tree().change_scene_to_packed(MAIN)
+		# get node
+		var Splash = get_node("2D element/Splash Screen")
+		var Menu = get_node("2D element/Menu")
+
+		Splash.hide()
+		Menu.show()
+func story_mode():
+	get_tree().change_scene_to_packed(MAIN)
+func quit_game():
+	get_tree().quit()
