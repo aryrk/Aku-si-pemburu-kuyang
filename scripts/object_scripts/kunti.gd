7fe87262fd03711c0495eb6b9f230726ae279cbc
aryrk <amanwhohave@gmail.com>
2024-05-20 19:20:10 +0700

    implemen proper jumpscare and sound easter egg

--- scripts/object_scripts/kunti.gd
+++ scripts/object_scripts/kunti.gd
@@ -12,9 +12,10 @@ func _process(delta):
 
 func _on_area_3d_body_entered(body):
 	if body.name == "Player":
-		print("Hello")
+		Audio.play("assets/sounds/Kunti Jumpscare.mp3")
+		Audio.play("assets/sounds/Rumore Cinnematic Impacts/HAL9K - Scream.wav")
 		jumpscare.show()
-		timer.wait_time = 3
+		timer.wait_time = 1
 		timer.start()
 	
 func remove_jumpscare():
