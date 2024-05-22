065b8d9b318b0d04e77ef6daffa52e22d723965a
aryrk <amanwhohave@gmail.com>
2024-05-22 13:32:02 +0700

    WIP 2

---
+++ scripts/scene/credit.gd
@@ -0,0 +1,15 @@
+extends Node3D
+@onready var credit := $"2D element/TextEdit"
+@onready var line := 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	pass
