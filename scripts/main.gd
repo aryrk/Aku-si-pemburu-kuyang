768b1297d7aea624a14b065b293c260c95fa28fb
aryrk <amanwhohave@gmail.com>
2024-05-24 10:40:39 +0700

    gameplay 2d element overhaull

--- scripts/main.gd
+++ scripts/main.gd
@@ -11,5 +11,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$"2D element/Helper".text = str(enemies.get_child_count()) + " kuyang tersisa"
 	if enemies and enemies.get_child_count() == 0:
-		get_tree().change_scene_to_file(WIN)
+		
+		$"2D element/AnimationTree".play("goal_achieved")
+
+
+func _on_animation_tree_animation_finished(anim_name):
+	if anim_name == "goal_achieved":
+		GameStat.release_mouse()
+		SceneSwitcher.change_scene("res://scenes/credit.tscn")
