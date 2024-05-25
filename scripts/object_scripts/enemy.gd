035a6bbe54b2af6255157292bd3422bad1567872
Matharrr <matharrizqi@gmail.com>
2024-05-25 13:00:39 +0700

    fix hitbox boss

--- scripts/object_scripts/enemy.gd
+++ scripts/object_scripts/enemy.gd
@@ -100,7 +100,7 @@ func _on_timer_timeout():
 	if raycast.is_colliding():
 		var collider = raycast.get_collider()
 		if collider.has_method("damage"):
-			collider.damage(10)
+			collider.damage(5)
 
 	# raycast.force_raycast_update()
 	# if raycast.is_colliding():
